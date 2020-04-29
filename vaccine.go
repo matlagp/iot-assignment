f2a8c76bd4e74f3b868699d324ec8881ba2b8531
frozanna <48160361+frozanna@users.noreply.github.com>
2020-04-29 20:07:27 +0200

    Data generator

--- vaccine.go
+++ vaccine.go
@@ -50,6 +50,50 @@ type QueryConclusionResult struct {
 }
 
 
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	led1 := Person { Name: "John", Surname: "Smith", Id: 1}
+	led2 := Person { Name: "Elizabeth", Surname: "Winter", Id: 2}
+	led3 := Person { Name: "Jim", Surname: "Badley", Id: 3}
+
+	res1 := []Person {Person { Name: "Fei", Surname: "Chu", Id: 4},
+						Person { Name: "Helena", Surname: "Gardner", Id: 5},
+						Person { Name: "Simon", Surname: "Sutton", Id: 6}}
+	res2 := []Person {Person { Name: "Milo", Surname: "Pacher", Id: 7},
+						Person { Name: "Andrew", Surname: "Human", Id: 8}}
+	res3 := []Person {Person { Name: "Nicolas", Surname: "Contino", Id: 9}}
+
+	t1s, _ = time.Parse(shortForm, "2020-Feb-03")
+	t1e, _ = time.Parse(shortForm, "2020-May-20")
+	t2s, _ = time.Parse(shortForm, "2018-Jan-15")
+	t2e, _ = time.Parse(shortForm, "2019-Jan-01")
+	t3s, _ = time.Parse(shortForm, "2017-Dec-05")
+	t3e, _ = time.Parse(shortForm, "2018-Nov-03")
+
+	exps := []VaccineExperiment{
+		VaccineExperiment{Company: "International Medical Laboratory", Description: "Testing vaccine for coronavirus",
+							Leader: led1, Researchers: res1, VaccineName: "VAC-COV-1", Disease: "COVID-19",
+							Conclusions := []Conclusion, StartTime := t1s, EndTime := t1e },
+						Company: "New York Medical Labolatory", Description: "Testing vaccine for new mutations of common flu.",
+							Leader: led2, Researchers: res2, VaccineName: "FLU-42-B", Disease: "Common flu",
+							Conclusions := []Conclusion, StartTime := t2s, EndTime := t2e },
+						Company: "California Main Hospital", Description: "Experimenting with vaccine for well known disease, which is laziness",
+							Leader: led3, Researchers: res3, VaccineName: "ANTI-LAZY-v0", Disease: "Laziness",
+							Conclusions := []Conclusion, StartTime := t3s, EndTime := t3e }
+	}
+
+	for i, exp := range exps {
+		expAsBytes, _ := json.Marshal(exp)
+		err := ctx.GetStub().PutState("EXP"+strconv.Itoa(i), expAsBytes)
+
+		if err != nil {
+			return fmt.Errorf("Failed to put to world state. %s", err.Error())
+		}
+	}
+
+	return nil
+}
+
+
 
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
 	return nil
